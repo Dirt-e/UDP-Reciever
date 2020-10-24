bcc4d9d809d236783d03eb1583831c306e55a005
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2020-10-24 22:57:38 +0200

    Initial commit

---
+++ UDP Reciever/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Reciever
+{
+    class Program
+    {
+        static void Main()
+        {
+            UdpClient receivingUdpClient = new UdpClient(10000);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+                string message = Encoding.ASCII.GetString(receiveBytes);
+
+                Console.WriteLine("You received: " + message.ToString());
+            }
+        }
+    }
+}
